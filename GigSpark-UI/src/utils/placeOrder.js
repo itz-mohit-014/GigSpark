7b611344116dbf0b2387d4fd5574c300c9e5278a
Mohit <mohitjangid38437@gmail.com>
2025-04-22 19:08:37 +0530

    fix razor pay payment test mode issue

--- GigSpark-UI/src/utils/placeOrder.js
+++ GigSpark-UI/src/utils/placeOrder.js
@@ -32,8 +32,8 @@ const handlePayment = async (productId, email) => {
 
 const handlePaymentVerify = async (orderDetails, name, gigId) => {
   const { VERIFY_PAYMENT } = Payment;
-  const RAZORPAY_KEY_ID = import.meta.env.RAZORPAY_KEY_ID;
-
+  const RAZORPAY_KEY_ID = import.meta.env.VITE_RAZORPAY_KEY_ID;
+  
   const options = {
     key: RAZORPAY_KEY_ID,
     amount: orderDetails.amount,
